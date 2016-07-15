8449ed43452ffe582ad1f21248fb828b7509c4a9
Smalicoat <sammalicoat@gmail.com>
2016-07-15 16:49:16 -0700

    merged!

--- plotter.c
+++ plotter.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-#include <dirent.h>
-#include <wiringPi.h>
-#include <unistd.h>
 #include <ncurses.h>
+#include <ncurses.h>
+
+#define WIDTH 30
+#define HEIGHT 10 
+#define TOPMENU 0
+#define FILESELECT 1
+#define PLOTDISPLAY 2
 
 #define RED   "\x1B[31m"
 #define GRN   "\x1B[32m"
@@ -13,147 +17,147 @@
 #define WHT   "\x1B[37m"
 #define RESET "\x1B[0m"
 /*
-	printf(RED "red\n" RESET);
-	printf(GRN "green\n" RESET);
-	printf(YEL "yellow\n" RESET);
-	printf(BLU "blue\n" RESET);
-	printf(MAG "magenta\n" RESET);
-	printf(CYN "cyan\n" RESET);
-	printf(WHT "white\n" RESET);
-*/	      
+ printf(RED "red\n" RESET);
+ printf(GRN "green\n" RESET);
+ printf(YEL "yellow\n" RESET);
+ printf(BLU "blue\n" RESET);
+ printf(MAG "magenta\n" RESET);
+ printf(CYN "cyan\n" RESET);
+ printf(WHT "white\n" RESET);
+*/
 
 
 bool quit = false;
-void clearScreen()
-{
-	const char* CLEAR_SCREE_ANSI = "\e[1;1H\e[2J";
-	write(STDOUT_FILENO,CLEAR_SCREE_ANSI,12);
-}
-
-int basicScreen()
-{
-	clearScreen();
-	init_pair(1,COLOR_BLACK,COLOR_WHITE);
-	attron(COLOR_PAIR(1));
-	move(0,0);
-	for(int i = 0; i < COLS;i++)
-		printw(" ");
-	mvprintw(0,COLS/2 - 5,"CNC PLOTTER");
-	attroff(COLOR_PAIR(1));
-	int letters;
-	mvprintw(4,COLS/2-56,"Hello and welcome to my creation. To start please select with arrow keys what mod you would like to start with.%n",&letters);
-	//printw(": %i",letters);
-	attron(COLOR_PAIR(1));
-	mvprintw(15,COLS/2-9,"[1] Plot File");
-	attroff(COLOR_PAIR(1));
-	mvprintw(16,COLS/2-9,"[2] Manual Control");
-	mvprintw(17,COLS/2-9,"[3] Exit");
-	move(LINES * .9,8);
-	int highlight = 0;
-	bool selected = false;
-	while(!quit)
-	{
-		int c = getch();
-		switch(c)
-		{
-			case KEY_UP:
-				highlight--;
-				if(highlight<1)
-					highlight = 1;
-				break;
-			case KEY_DOWN:
-				highlight++;
-				if(highlight > 3)
-					highlight = 3;
-				break;
-			case 113:
-			case 81:
-		//	case 27:
-				quit = true;
-				break;
-			
-			case 10:
-				selected = true;
-				break;
-			default:
-				printw("did not reconize the command: %c",c);
-				break;
-		}
-		attron(COLOR_PAIR(1));
-		switch(highlight)
-		{
-			case 1:
-				mvprintw(15,COLS/2-9,"[1] Plot File");
-				break;
-			case 2:
-				mvprintw(16,COLS/2-9,"[2] Manual Control");
-				break;
-			case 3:
-				mvprintw(17,COLS/2-9,"[3] Exit");
-				break;
-			default:
-			break;
-		}
-		attroff(COLOR_PAIR(1));
-
-	}
-	if(quit)
-		return 0;
-	printw("you selected option: %i",highlight);
-	refresh();
-	return 1;
-}
+void clearScreen();
+void print_menu(WINDOW *menu_win, int highlight,char * choices[][2], int n_choices,int startx,int starty);
+int menus(char * choices[][2],int n_choices);
 int main()
 {
-
-	//wiringPiSetupGpio(): //need to do this once i start working with the
-		//pi need to look into what pin number sceme to use
-
-	initscr();	
-	start_color();
+       	initscr();
+	clear();
 	noecho();
 	cbreak();
-/*	FILE * fp;
-	char * line = NULL;
-	size_t len = 0;
-	ssize_t read;
+	start_color();
+	init_pair(1,COLOR_BLACK,COLOR_WHITE);
+        attron(COLOR_PAIR(1));
+        move(0,0);
+        for(int i = 0; i < COLS;i++)
+                printw(" ");
+        mvprintw(0,COLS/2 - 5,"CNC PLOTTER");
+        attroff(COLOR_PAIR(1));
 
-	fp = fopen ("sampleGcode.txt","r");
-	if(fp == NULL)
-	{
-		perror("open failed");
-		return 2;
-	}
-	while((read = getline(&line, &len, fp)) != -1)
-	{
-		printw("%s",line);
-		refresh();
-	}
-	
-	fclose(fp);*/
-//	if(line)
-//		free(line);
-	
-/*	DIR *dp;
-	struct dirent *ep;
-	dp = opendir ("./");
-	if(dp != NULL)
-	{
-		while (ep = readdir (dp))
-			if(ep->d_name[0]!='.')
-				printw("%s  ",ep->d_name);
-		(void) closedir(dp);
-		refresh();
-	}
-	else 
-		perror("Could't open the directory");
-		*/
-	basicScreen();
-	while(true)
-	{
-	}
-	getch();	
+	char  *choices[][2] = { 
+			{"Plot File","You select a file to plot and will plot the file on the plotter"},
+			{"Manual Control","Will give you full control over the plotter for debuging or demenstration"},
+			{"Exit","Will exit the prgram"},
+			};
+/* Line buffering disabled. pass on everything */
+	int n_choices = sizeof(choices) / (2*sizeof(char *));
+	//mvprintw(3,4,"the vale of sizeof(choices):%d\nvale of sieof(char *):%d\nsize of sizeof(*choices):%d\nsize of n_choices:%d",sizeof(choices),sizeof(char *),sizeof(*choices),n_choices);
+//	printw("\nthe first two strings are :%s \n and %s",choices[0][0],choices[0][1]);
+	menus(choices,n_choices);
 	endwin();
 	return 0;
 }
+//switch(option)
+//	{
+//		case PLOTDISPLAY:
+//			char *choices[] = { 
+//				"Plot DISPLAY",
+//				"INSTERT HERE",
+//				"Exit",
+//				};
+//		break;
+//		case FILESELECT:
+//			char *choices[] = { 
+//				"FILE SELECT",
+//				"INSTERT HERE",
+//				"Exit",
+//				};
+//		break;
+//		default:
+//			char *choices[] = { 
+//				"Plot File",
+//				"Manual Control",
+//				"Exit",
+//				};
 
+//			//		break;
+//	}
+int menus(char* choices[][2],int n_choices)
+	{
+	WINDOW *menu_win;
+	int highlight = 1;
+	int choice = 0;
+	int c;
+
+	int startx = (80 - WIDTH) / 3;
+	int starty = 4+(24 - HEIGHT) / 3;
+
+	menu_win = newwin(HEIGHT, WIDTH, starty, startx);
+	keypad(menu_win, TRUE);
+        mvprintw(2,2,"Hello and welcome to my creation. To start please select with the arrow keys");
+	//mvprintw(4, 0, "Use arrow keys to go up and down, Press enter to select a choice");
+	refresh();
+	
+	print_menu(menu_win, highlight,choices,n_choices,startx,starty);
+	while(1)
+	{
+	c = wgetch(menu_win);
+		switch(c)
+		{
+			case KEY_UP:
+				if(highlight == 1)
+					highlight = n_choices;
+				else
+					--highlight;
+				break;
+			case KEY_DOWN:
+				if(highlight == n_choices)
+					highlight = 1;
+				else 
+					++highlight;
+				break;
+			case 10:
+				choice = highlight;
+				break;
+			default:
+				mvprintw(24, 0, "Charcter pressed is = %3d Hopefully it can be printed as '%c'", c, c);
+				refresh();
+				break;
+		}
+		print_menu(menu_win, highlight,choices,n_choices,startx,starty);
+		if(choice != 0)	/* User did a choice come out of the infinite loop */
+			break;
+	}	
+	mvprintw(23, 0, "You chose choice %d with choice string %s\n", choice, choices[choice - 1]);
+	clrtoeol();
+	refresh();
+	delwin(menu_win);
+	return choice;
+}
+
+
+void print_menu(WINDOW *menu_win, int highlight,char * choices[][2],int n_choices,int startx,int starty)
+{
+	int x, y, i;	
+
+	x = 2;
+	y = 2;
+	for(i = 0; i < n_choices; ++i)
+	{
+		if(highlight == i + 1) /* High light the present choice */
+		{
+			wattron(menu_win, A_REVERSE); 
+			mvwprintw(menu_win, y, x, "%s", choices[i][0]);
+			wattroff(menu_win, A_REVERSE);
+		}
+		else
+			mvwprintw(menu_win, y, x, "%s", choices[i][0]);
+		++y;
+	}
+	WINDOW *descript_win = newwin(HEIGHT, WIDTH+4, starty, startx+WIDTH+2);
+	mvwprintw(descript_win, 2,2,"%s",choices[highlight -1][1]);
+	wrefresh(menu_win);
+	wrefresh(descript_win);
+}
